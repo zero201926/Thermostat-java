665a66a2401913c0dc5f00988158879c708dd694
vikash <zero_transfer@outlook.com>
2019-10-29 14:09:32 +0000

    9 + 10 to do

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,42 +1,53 @@
+'use strict';
 describe("Thermostat", function() {
   var thermostat;
 
   beforeEach(function() {
-    thermostat= new Thermostat();
+    thermostat = new Thermostat();
   });
-  
+
   describe("temperature options", function() {
     it("Thermostat starts at 20 degrees", function() {
-      expect(thermostat._temp).toEqual(20);
+      expect(thermostat.current_temp()).toEqual(20);
     });
     it("You can increase the temperature", function() {
-      expect(thermostat._temp).toEqual(20);
+      thermostat.up();
+      expect(thermostat.current_temp()).toEqual(21);
     });
     it("You can decrease the temperature", function() {
-      expect(thermostat._temp).toEqual(20);
+      thermostat.down();
+      expect(thermostat.current_temp()).toEqual(19);
     });
     it("The minimum temperature is 10 degrees", function() {
-      expect(thermostat._temp).toEqual(20);
+      expect(thermostat._min_temp).toEqual(10);
     });
   });
 
   describe("power saving mode on", function() {
     it("Power saving mode is on by default", function() {
-      expect(thermostat._temp).toEqual(20);
+      expect(thermostat._power_saving_mode).toEqual(true);
     });
     it("the maximum temperature is 25 degrees", function() {
-      expect(thermostat._temp).toEqual(20);
+
+      expect(thermostat.max_temp()).toEqual(25);
     });
   });
 
   describe("power saving mode off", function() {
+    // thermostat = jasmine.createSpyObj('_power_saving_mode', [false])
+    // thermostat._power_saving_mode = false
     it("the maximum temperature is 32 degrees", function() {
-      expect(thermostat._temp).toEqual(20);
+      console.log(thermostat._power_saving_mode)
+      thermostat.power_saving_button();
+      console.log(thermostat._power_saving_mode)
+      expect(thermostat.max_temp()).toEqual(32);
     });
   });
 
   describe("#reset", function() {
     it("You can reset the temperature to 20", function() {
+      thermostat.up();
+      thermostat.reset();
       expect(thermostat._temp).toEqual(20);
     });
   });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,24 +1,41 @@
+'use strict';
 function Thermostat() {
 
   this._min_temp = 10;
   this._temp = 20;
   this._power_saving_mode = true;
+}
 
-  Thermostat.max_temp = function() {
-    if(Thermostat._power_saving_mode = true){
-      return 25;
-    }
+Thermostat.prototype.power_saving_button = function() {
+  if(this._power_saving_mode == true) {
+    this._power_saving_mode = false;
+  } else {
+    this._power_saving_mode = true;
+  };
+};
+
+Thermostat.prototype.max_temp = function() {
+  if(this._power_saving_mode == true){
+    return 25;
+  } else {
     return 32;
   };
-}
-Thermostat .prototype.up = function(song) {
-  ++Thermostat._temp
 };
 
-Thermostat .prototype.down = function() {
-  --Thermostat._temp
+Thermostat.prototype.current_temp = function() {
+  return this._temp;
 };
 
-Thermostat .prototype.reset= function() {
-  Thermostat._temp = 20;
+Thermostat.prototype.up = function() {
+  ++this._temp
+  //return this.temp;
+};
+
+Thermostat.prototype.down = function() {
+  --this._temp
+  //return this.temp;
+};
+
+Thermostat.prototype.reset= function() {
+  this._temp = 20;
 };
